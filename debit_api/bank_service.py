f965d26beb85a0b16088edd052383f2a5d2abeb8
Jordan Bushnell <jordan.bushnell@outlook.com>
2023-05-13 22:10:51 +0100

    Use actual bank service

--- debit_api/bank_service.py
+++ debit_api/bank_service.py
@@ -19,8 +19,6 @@ class BankService:
         
     def requestPayment(amount, currency, recipientAccount, bookingId):
         req = BANK_URL+'/bank/pay'
-        return True
-        
         try:
             response = requests.post(req, json={
                 'amount': amount,
@@ -34,8 +32,6 @@ class BankService:
         
     def requestRefund(bookingId):
         req = BANK_URL+'/bank/refund'
-        
-        return True
         try:
             response = requests.post(req, json={
                 'bookingId': bookingId
