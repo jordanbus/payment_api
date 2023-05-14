fa1e849665d8daea86b837ff9da26dbdfde87afc
Jordan Bushnell <jordan.bushnell@outlook.com>
2023-05-14 21:46:22 +0100

    Remove salt field from admin form and use radio field for card type

--- debit_api/admin.py
+++ debit_api/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
 from .models import Card, Billing, Transaction, Currency
+from django import forms
+class CardForm(forms.ModelForm):
+    cardType = forms.ChoiceField(
+        choices=[('Visa', 'Visa'), ('MasterCard', 'MasterCard')],
+        widget=forms.RadioSelect
+    )
+    
+    class Meta:
+        model = Card
+        fields = '__all__'
 
-# Register your models here.
-admin.site.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    form = CardForm
+    list_display = (
+        'cardNumber',
+        'cardType',
+        'cvv',
+        'expiryDate', 
+        'name', 
+        'balance',
+        'accountCurrency',
+        'email',
+    )
+    # Specify other customization options as needed
+    def get_exclude(self, request, obj=None):
+        exclude = super().get_exclude(request, obj)
+        if exclude is None:
+            exclude = []
+        exclude.append('salt')
+        return exclude
+    
+# Register the Card model with the custom admin class
+admin.site.register(Card, CardAdmin)
 admin.site.register(Billing)
 admin.site.register(Transaction)
 admin.site.register(Currency)
